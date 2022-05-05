6ceda44f7261ca848e6d001415f5adf30d8319e8
TasyaKh <tasya132002@gmail.com>
2022-05-05 18:49:25 +0800

    добавили поля (что то вроде индексов), нужно для сохранения настроек в дальнейшем

--- app/src/main/java/com/example/bd/Logic/LanguageWord.java
+++ app/src/main/java/com/example/bd/Logic/LanguageWord.java
@@ -1,7 +1,16 @@
 package com.example.bd.Logic;
 
 public enum LanguageWord {
-    ENGLISH,
-    RUSSIAN,
+    ENGLISH(0),
+    RUSSIAN(1);
+    private final int numEnum;
+
+    LanguageWord(int numEnum){
+        this.numEnum = numEnum;
+    }
+
+    public int getNumEnum() {
+        return numEnum;
+    }
 }
 
--- app/src/main/java/com/example/bd/Logic/SortWord.java
+++ app/src/main/java/com/example/bd/Logic/SortWord.java
@@ -1,7 +1,16 @@
 package com.example.bd.Logic;
 
 public enum SortWord {
-    LAST_DATA,
-    FIRST_DATA,
-    NAME,
+    DEFAULT(0),
+    FIRST_DATA(1),
+    NAME(2);
+    private final int numEnum;
+
+    SortWord(int numEnum){
+        this.numEnum = numEnum;
+    }
+
+    public int getNumEnum() {
+        return numEnum;
+    }
 }
